de0b740c9609e68dbbdb175a484a7268e2ecdacc
slinky-snake <isabellebeariz07@gmail.com>
2025-07-08 14:34:39 -0300

    Add files via upload

---
+++ dsf010(dolar).py
@@ -0,0 +1,4 @@
+print('conversor de dólares!! \n>>>')
+valor=float(input('quantos reais vc tem?R$'))
+soma=valor * 5.81
+print('você tem US${:.2f} dólares no bolso, tudo culpa do trump!'.format(soma))
